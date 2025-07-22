92c89310dbbf032bb65c9d69c53f3845fc8b1639
Anton <lastakkaynt@gmail.com>
2025-07-22 11:24:53 +0300

    add flowbite styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,28 +1,31 @@
 module.exports = {
-  darkMode: 'class',
-  content: [
-  './templates/**/*.html',
-  './aikademiya/**/*.py',
-  './assets/**/*.css',
-  './static/**/*.css',
-  ],
-  theme: {
-    extend: {
-      colors: {
-        primary: {
-          50: '#eff6ff',
-          100: '#dbeafe',
-          200: '#bfdbfe',
-          300: '#93c5fd',
-          400: '#60a5fa',
-          500: '#3b82f6',
-          600: '#2563eb',
-          700: '#1d4ed8',
-          800: '#1e40af',
-          900: '#1e3a8a',
+    darkMode: 'class',
+    content: [
+        './templates/**/*.html',
+        './aikademiya/**/*.py',
+        './assets/**/*.css',
+        './static/**/*.css',
+        './node_modules/flowbite/**/*.js'
+    ],
+    theme: {
+        extend: {
+            colors: {
+                primary: {
+                    50: '#eff6ff',
+                    100: '#dbeafe',
+                    200: '#bfdbfe',
+                    300: '#93c5fd',
+                    400: '#60a5fa',
+                    500: '#3b82f6',
+                    600: '#2563eb',
+                    700: '#1d4ed8',
+                    800: '#1e40af',
+                    900: '#1e3a8a',
+                },
+            },
         },
-      },
     },
-  },
-  plugins: [],
+    plugins: [
+        require('flowbite/plugin')
+    ],
 };
