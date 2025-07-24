6d1f4f6c3fddece01f4b9a05dab0a7de11bb8768
Anton <lastakkaynt@gmail.com>
2025-07-24 10:11:16 +0300

    fix settings.py and add .gitignore

--- aikademiya/aikademiya/settings.py
+++ aikademiya/aikademiya/settings.py
@@ -98,8 +98,12 @@ WSGI_APPLICATION = "aikademiya.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("POSTGRES_DB", "aikademiya"),
+        "USER": os.environ.get("POSTGRES_USER", "aikademiya"),
+        "PASSWORD": os.environ.get("POSTGRES_PASSWORD", "aikademiya"),
+        "HOST": os.environ.get("POSTGRES_HOST", "localhost"),
+        "PORT": os.environ.get("POSTGRES_PORT", "5432"),
     }
 }
 
